2d73880757e2a1eed7d55abeb4a4f4cd3ed89b9c
BillyFlex <59295910+BillyFlex@users.noreply.github.com>
2026-09-01 00:34:36 +0200

    Increase sleep duration before sending commands

--- opnsense-vm.sh
+++ opnsense-vm.sh
@@ -811,7 +811,7 @@ msg_ok "Bridge interfaces have been successfully added."
 msg_ok "Created a OPNsense VM ${CL}${BL}(${HN})"
 msg_ok "Starting OPNsense VM (Patience this takes 20-30 minutes)"
 qm start $VMID
-sleep 120
+sleep 150
 send_line_to_vm "root"
 sleep 2
 send_line_to_vm "ifconfig vtnet0 10.59.0.3/29"
